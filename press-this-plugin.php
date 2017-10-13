d966a6a57a9db1cd16bf30c924f5751ac2df593f
Brandon Kraft <public@brandonkraft.com>
2017-10-13 11:14:18 -0500

    Initial Git commit

---
+++ press-this-plugin.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Press This
+ *
+ * Plugin Name: Press This
+ * Plugin URI:  https://wordpress.org
+ * Description: A little tool that lets you grab bits of the web and create new posts with ease.
+ * Version:     0.2.0
+ * Author:      WordPress Contributors
+ * Author URI:  https://wordpress.org
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Text Domain: press-this
+ * Domain Path: /languages
+ */
+ /*
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License version 2, as published by the Free Software Foundation.  You may NOT assume
+ * that you can use any other version of the GPL.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ */
+
+add_action( 'wp_ajax_press-this-plugin-save-post', 'wp_ajax_press_this_plugin_save_post');
+add_action( 'wp_ajax_press-this-plugin-add-category', 'wp_ajax_press_this_plugin_add_category' );
+add_action( 'tool_box', 'press_this_tool_box' );
+
+ /**
+ * Ajax handler for saving a post from Press This.
+ *
+ * @since 1.0.0
+ */
+function wp_ajax_press_this_plugin_save_post() {
+	include( plugin_dir_path( __FILE__ ) . 'class-wp-press-this-plugin.php' );
+	$wp_press_this = new WP_Press_This_Plugin();
+	$wp_press_this->save_post();
+}
+
+/**
+ * Ajax handler for creating new category from Press This.
+ *
+ * @since 1.0.0
+ */
+function wp_ajax_press_this_plugin_add_category() {
+	include( plugin_dir_path( __FILE__ ) . 'class-wp-press-this-plugin.php' );
+	$wp_press_this = new WP_Press_This_Plugin();
+	$wp_press_this->add_category();
+}
+
+function press_this_tool_box() {
+	if ( current_user_can('edit_posts') ) { ?>
+		<div class="card pressthis">
+			<h2><?php _e('Press This') ?></h2>
+			<p><?php _e( 'Press This is a little tool that lets you grab bits of the web and create new posts with ease.' ); ?>
+				<?php _e( 'It will even allow you to choose from images or videos included on the page and use them in your post.' ); ?>
+				<?php _e( 'Use Press This as a quick and lightweight way to highlight another page on the web.' ); ?></p>
+
+			<p><a href="<?php echo htmlspecialchars( admin_url( 'press-this.php' ) ); ?>"><?php _e( 'Open Press This' ); ?></a>
+				<?php _e( 'then add it to your device&#8217;s bookmarks or home screen.' ); ?></p>
+		</div>
+	<?php }
+}
---
+++ readme.txt
@@ -0,0 +1,26 @@
+=== Press This ===
+Contributors: kraftbj
+Donate link: http://wordpressfoundation.org/donate/
+Tags: post, quick-post, photo-post
+Requires at least: 4.9
+Tested up to: 4.9
+Stable tag: 0.2.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Posting images, links, and cat gifs will never be the same.
+
+== Description ==
+
+Press This is a little tool that lets you grab bits of the web and create new posts with ease.
+It will even allow you to choose from images or videos included on the page and use them in your post.
+Use Press This as a quick and lightweight way to highlight another page on the web.
+
+== Contributing ==
+
+It'll be on GitHub shortly.
+
+== Changelog ==
+
+= 1.0 =
+* When Tagged: Initial release
