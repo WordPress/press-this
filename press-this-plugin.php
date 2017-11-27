54ab3d4d924a8711370349c50243abe7169ab38f
Brandon Kraft <public@brandonkraft.com>
2017-11-27 16:10:25 -0600

    Restore install instructions

--- press-this-plugin.php
+++ press-this-plugin.php
@@ -55,10 +55,57 @@ function press_this_tool_box() {
 			<h2><?php _e('Press This', 'press-this') ?></h2>
 			<p><?php _e( 'Press This is a little tool that lets you grab bits of the web and create new posts with ease.', 'press-this' ); ?>
 				<?php _e( 'It will even allow you to choose from images or videos included on the page and use them in your post.', 'press-this' ); ?>
-				<?php _e( 'Use Press This as a quick and lightweight way to highlight another page on the web.', 'press-this' ); ?></p>
+				<?php _e( 'Use Press This as a quick and lightweight way to highlight another page on the web.', 'press-this' ); ?>
+			</p>
 
-			<p><a href="<?php echo htmlspecialchars( admin_url( 'press-this.php' ) ); ?>"><?php _e( 'Open Press This', 'press-this' ); ?></a>
-				<?php _e( 'then add it to your device&#8217;s bookmarks or home screen.', 'press-this' ); ?></p>
+			<form>
+				<h3><?php _e( 'Install Press This' ); ?></h3>
+				<h4><?php _e( 'Direct link (best for mobile)' ); ?></h4>
+				<p><a href="<?php echo htmlspecialchars( admin_url( 'press-this.php' ) ); ?>"><?php _e( 'Open Press This', 'press-this' ); ?></a>
+					<?php _e( 'then add it to your device&#8217;s bookmarks or home screen.', 'press-this' ); ?>
+				</p>
+
+				<h4><?php _e( 'Bookmarklet' ); ?></h4>
+				<p><?php _e( 'Drag the bookmarklet below to your bookmarks bar. Then, when you&#8217;re on a page you want to share, simply &#8220;press&#8221; it.' ); ?></p>
+
+				<p class="pressthis-bookmarklet-wrapper">
+					<a class="pressthis-bookmarklet" onclick="return false;" href="<?php echo htmlspecialchars( press_this_get_shortcut_link() ); ?>"><span><?php _e( 'Press This' ); ?></span></a>
+					<button type="button" class="button pressthis-js-toggle js-show-pressthis-code-wrap" aria-expanded="false" aria-controls="pressthis-code-wrap">
+						<span class="dashicons dashicons-clipboard"></span>
+						<span class="screen-reader-text"><?php _e( 'Copy &#8220;Press This&#8221; bookmarklet code' ) ?></span>
+					</button>
+				</p>
+
+				<div class="hidden js-pressthis-code-wrap clear" id="pressthis-code-wrap">
+					<p id="pressthis-code-desc">
+						<?php _e( 'If you can&#8217;t drag the bookmarklet to your bookmarks, copy the following code and create a new bookmark. Paste the code into the new bookmark&#8217;s URL field.' ) ?>
+					</p>
+
+					<p>
+						<textarea class="js-pressthis-code" rows="5" cols="120" readonly="readonly" aria-labelledby="pressthis-code-desc"><?php echo htmlspecialchars( press_this_get_shortcut_link() ); ?></textarea>
+					</p>
+				</div>
+
+				<script>
+				jQuery( document ).ready( function( $ ) {
+					var $showPressThisWrap = $( '.js-show-pressthis-code-wrap' );
+					var $pressthisCode = $( '.js-pressthis-code' );
+
+					$showPressThisWrap.on( 'click', function( event ) {
+						var $this = $( this );
+
+						$this.parent().next( '.js-pressthis-code-wrap' ).slideToggle( 200 );
+						$this.attr( 'aria-expanded', $this.attr( 'aria-expanded' ) === 'false' ? 'true' : 'false' );
+					});
+
+					// Select Press This code when focusing (tabbing) or clicking the textarea. 
+					$pressthisCode.on( 'click focus', function() {
+						var self = this;
+						setTimeout( function() { self.select(); }, 50 );
+					});
+				});
+				</script>
+			</form>
 		</div>
 	<?php }
 }
