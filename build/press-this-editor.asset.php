56ee6590ea7ef9e54f5ed6615366e77f00c5003a
Brandon Kraft <public@brandonkraft.com>
2026-01-12 11:19:22 -0500

    Exclude build directory from version control
    Add /build/ to .gitignore and remove tracked build artifacts.

--- build/press-this-editor.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-block-library', 'wp-blocks', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '0e4cbefb4fe3fe2ef544');
